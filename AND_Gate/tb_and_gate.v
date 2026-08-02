03b2d13fbc7ae4d78a0df666593fa9c1605299c7
himanshuprajapati738858-lgtm <himanshuprajapati738858@gmail.com>
2026-08-02 18:58:54 +0530

    Add testbench for AND gate
    Added  the verilog testbentch for functional simulation of the AND gate

---
+++ AND_Gate/tb_and_gate.v
@@ -0,0 +1,22 @@
+`timescale 1ns / 1ps
+
+module tb_and_gate;
+
+reg A, B;
+wire Y;
+
+and_gate uut (
+   .A(A),
+   .B(B),
+   .Y(Y)
+);
+
+initial begin 
+
+    A=0; B=0; #10;
+    A=0; B=1; #10;
+    A=1; B=0; #10;
+    A=1; B=1; #10;
+   $finish; #10;
+end
+endmodule
